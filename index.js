03a7893bfa248fba0ba8f3e65ad9a12f2fa5ab5c
gurpreet7singh <7gaming147@gmail.com>
2024-11-06 11:12:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+var a=document.querySelectorAll(".drum")
+for(i=0; i<a.length; i++){
+    a[i].addEventListener("click", function (){
+        var buttonInnerHtml = this.innerHTML;
+        playSound(buttonInnerHtml)
+    })
+}
+
+document.addEventListener("keydown",function(event){
+    playSound(event.key)
+});
+
+function playSound(key){
+
+    switch(key){
+        case "w":
+            let crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        
+        case "a":
+            let kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        case "s":
+            let snare=new Audio ("./sounds/snare.mp3");
+            snare.play()
+            break;
+        
+        case "d":
+            let tom1=new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+            
+        case "j":
+            let tom2=new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "k":
+            let tom3= new Audio("./sounds/tom-3.mp3")
+            tom3.play();
+            break;
+
+        case "l":
+            let tom4=new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        
+        default: console.log('wrong input')
+    }
+    
+}
